ec9002afcf170b4bf4e8d01599f8208f90137d01
McFog Wang <mcfogw@gmail.com>
2018-09-19 00:27:36 +0800

    fix

--- src/Lit/Bolt/Middlewares/ContextMiddleware.php
+++ src/Lit/Bolt/Middlewares/ContextMiddleware.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Lit\Bolt\Middlewares;
 
+use Lit\Nimo\Traits\AttachToRequestTrait;
+use Lit\Nimo\Traits\MiddlewareCompositeTrait;
 use Lit\Nimo\Traits\MiddlewareTrait;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -11,6 +13,8 @@ use Psr\Http\Server\MiddlewareInterface;
 class ContextMiddleware extends \ArrayObject implements MiddlewareInterface
 {
     use MiddlewareTrait;
+    use AttachToRequestTrait;
+    use MiddlewareCompositeTrait;
 
     public function __construct(array $input = [])
     {
