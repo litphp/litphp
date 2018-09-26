a02164e311d4634d49df5ca4fa7f1833b18f448d
McFog Wang <mcfogw@gmail.com>
2018-09-26 21:41:38 +0800

    runner::run allow accept container interface

--- src/Lit/Runner/ZendSapi/BoltZendRunner.php
+++ src/Lit/Runner/ZendSapi/BoltZendRunner.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Lit\Runner\ZendSapi;
 
 use Lit\Bolt\BoltContainerConfiguration;
+use Psr\Container\ContainerInterface;
 use Zend\HttpHandlerRunner\RequestHandlerRunner;
 
 class BoltZendRunner
 {
     public static function run($config = [])
     {
-        $container = BoltContainerConfiguration::createContainer($config + BoltZendConfiguration::default());
+        $container = $config instanceof ContainerInterface
+            ? $config
+            : BoltContainerConfiguration::createContainer($config + BoltZendConfiguration::default());
+
         /** @var RequestHandlerRunner $runner */
         $runner = $container->get(RequestHandlerRunner::class);
         $runner->run();
